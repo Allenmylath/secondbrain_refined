84f74c82387e1ffa6419c9893b09d9f98735da6c
allenmylath <allengeorgemylath@gmail.com>
2025-07-29 16:29:56 +0530

    Create __init__.py

---
+++ src/database/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2025, Daily
+#
+# SPDX-License-Identifier: BSD 2-Clause License
+#
+
+"""Database module for real estate bot."""
+
+from .mongodb_client import PropertyDatabase
+
+__all__ = ["PropertyDatabase"]
