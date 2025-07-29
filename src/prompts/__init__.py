80f60d84b07ee1fc8ee506ffb060f38fee997ba9
allenmylath <allengeorgemylath@gmail.com>
2025-07-29 16:31:04 +0530

    Create __init__.py

---
+++ src/prompts/__init__.py
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2025, Daily
+#
+# SPDX-License-Identifier: BSD 2-Clause License
+#
+
+"""Prompts module for the real estate bot."""
+
+from .system_prompts import (
+    BOT_SYSTEM_PROMPT,
+    STRANDS_AGENT_PROMPT,
+    WELCOME_MESSAGE,
+    ERROR_PROMPTS,
+)
+
+__all__ = [
+    "BOT_SYSTEM_PROMPT",
+    "STRANDS_AGENT_PROMPT", 
+    "WELCOME_MESSAGE",
+    "ERROR_PROMPTS",
+]
